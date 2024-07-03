00b36e65fcd579427344a6529827476050c518c4
brookiestein <this.brayan@proton.me>
2024-07-03 11:33:38 -0400

    Initial Project Files.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "BtScreenLocker_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    return a.exec();
+}
